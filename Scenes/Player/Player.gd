c1fee0d9db20ba1a6aa7daea0a4885e450773368
andrzej.witkowski <andrzej.witkowski@gmail.com>
2025-12-28 20:18:24 +0100

    Refactor player movement and sound handling for improved gameplay experience

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -47,20 +47,67 @@ func _enter_tree() -> void:
 
 #region Movement
 func _physics_process(delta: float) -> void:
+	handle_rotation()
+	handle_gravity_and_jump(delta)
+	handle_sounds()
+	handle_movement(delta)	
 	
+	
+func handle_rotation() -> void:
 	rotate_y(-_look_delta.x * sensitivity)
 	camera_3d.rotate_x(-_look_delta.y * sensitivity)
 	camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-50), deg_to_rad(50))
 	_look_delta = Vector2.ZERO
 	
+func handle_movement(delta: float) -> void:
+	
+	var input_dir: Vector2 = Input.get_vector("left", "right", "forward", "back")
+	var direction: Vector3 = (
+		transform.basis * Vector3(input_dir.x, 0, input_dir.y)
+	).normalized()
+	
+	if direction != Vector3.ZERO:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = lerp(velocity.x, 0.0, delta * 6)
+		velocity.z = lerp(velocity.z, 0.0, delta * 6)
+	
+	move_and_slide()
+	
+func handle_gravity_and_jump(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
-	move_and_slide()
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed 
+	
 #endregion
 
-
 #region Sounds
+
+func handle_sounds() -> void:
+	var is_moving = is_on_floor() and velocity.length() > 0.5
+	
+	play_sounds(is_moving)
+	
+	_was_moving = is_moving
+	_was_on_floor = is_on_floor()
+	
+func play_sounds(is_moving: bool) -> void:
+	if !_was_on_floor and is_on_floor():
+		landing_sound.play()
+		if is_moving:
+			play_footsteps()
+	elif _was_on_floor and !is_on_floor():
+		play_jump_sound()
+	elif is_moving != _was_moving:
+		if is_moving:
+			play_footsteps()
+		else:
+			play_stop_sound()
+		
+	pass
+
 func play_footsteps() -> void:
 	walking_sound.stream = CONCRETE_FOOTSTEPS_LOOP
 	walking_sound.play()
